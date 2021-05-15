d20b58a43903e892883c1403abd907164e063b8d
carlinjshaw <carlinjshaw@gmail.com>
2021-05-15 18:24:15 -0400

    added timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,32 @@
-var initbutton = document.querySelector("#idbtn")
+var initbutton = document.querySelector("#idbtn");
+var timerEl = document.getElementById('countdown');
+
 
 initbutton.onclick = function () {
 
     window.alert("The button was pressed");
+
+
+
+    function countdown() {
+        var timeLeft = 60;
+      
+        var timeInterval = setInterval(function() {
+          if (timeLeft > 1) {
+            timerEl.textContent = timeLeft + ' seconds remaining';
+            timeLeft--;
+          } else if (timeLeft === 1) {
+            timerEl.textContent = timeLeft + ' second remaining';
+            timeLeft--;
+          } else {
+            timerEl.textContent = '';
+            clearInterval(timeInterval);
+            displayMessage();
+          }
+        }, 1000);
+      }
+countdown()
+
+
+
 }
