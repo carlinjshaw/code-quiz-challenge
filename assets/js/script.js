a9bca14ba0e23aec6958fc6121db76c48e1c607f
carlinjshaw <carlinjshaw@gmail.com>
2021-05-15 21:48:03 -0400

    made the question answers button and added event listeners to respond to clicks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,8 @@
 var initbutton = document.querySelector("#idbtn");
 var timerEl = document.getElementById('countdown');
-
+var body = document.body
 
 initbutton.onclick = function () {
-
-    window.alert("The button was pressed");
-
-
 
     function countdown() {
         var timeLeft = 60;
@@ -21,12 +17,63 @@ initbutton.onclick = function () {
           } else {
             timerEl.textContent = '';
             clearInterval(timeInterval);
-            displayMessage();
+         
           }
         }, 1000);
       }
 countdown()
 
+function question1 () {
+//question
+var divEl = document.createElement ("div");
+var h2El = document.createElement ("h2");
+h2El.textContent = "Which of the following is NOT a wizard in the LOTR universe?";
+divEl.appendChild(h2El);
 
+//button answers
+var optionsEl = document.createElement ("ol");
+var btn1 = document.createElement ("button");
+var btn2 = document.createElement ("button");
+var btn3 = document.createElement ("button");
+var btn4 = document.createElement ("button");
+btn1.textContent = "Mithrandir";
+btn2.textContent = "Blue wizard";
+btn3.textContent = "Radagast";
+btn4.textContent = "Dumbledore";
+//where buttun answers go
+body.appendChild(divEl);
+divEl.appendChild(optionsEl);
+optionsEl.appendChild(btn1);
+optionsEl.appendChild(btn2);
+optionsEl.appendChild(btn3);
+optionsEl.appendChild(btn4);
+
+//what button answers do 
+btn4.addEventListener('click', function() {
+    window.alert("that is correct!")
+    return question1(); 
+//add a point to score
+  });
+  btn4.addEventListener('click', function() {
+    window.alert("that is correct!")
+//subtract time to timer
+  });
+  btn1.addEventListener('click', function() {
+    window.alert("that is wrong!")
+//subtract time to timer
+  });
+  btn2.addEventListener('click', function() {
+    window.alert("that is wrong!")
+//subtract time to timer
+  });
+  btn3.addEventListener('click', function() {
+    window.alert("that is wrong!")
+//subtract time to timer
+  });
+
+}
+question1 ();
+
+// window.alert ("the previous question is over? Maybe?")
 
 }
