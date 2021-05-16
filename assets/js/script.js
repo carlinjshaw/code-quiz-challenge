ee0ed3678c218b5a70f08ce3c44ba27a750d1d01
carlinjshaw <carlinjshaw@gmail.com>
2021-05-16 18:14:01 -0400

    made score keeper work and start button a DOM object

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,22 @@
-var initbutton = document.querySelector("#idbtn");
+// var initbutton = document.querySelector("#idbtn");
 var timerEl = document.getElementById('countdown');
 var body = document.body
 
-initbutton.onclick = function () {
-    var timeLeft = 60;
 
+var initbutton = document.createElement("button");
+body.appendChild(initbutton);
+initbutton.textContent = ("Commence the Journey");
+initbutton.setAttribute('style', 'display: block; margin: auto; border-width: 3px; border-radius: 5px;');
+
+
+// infoEl.setAttribute('style', 'margin:auto; width:50%; text-align:center;');
+
+initbutton.onclick = function () {
+ 
+body.removeChild(initbutton);
+
+//timer  
+    var timeLeft = 60;
     function countdown() {
       
         var timeInterval = setInterval(function() {
@@ -23,8 +35,18 @@ initbutton.onclick = function () {
       }
 countdown()
 
+//score count
+var scoreEl = document.createElement ("h3")
+var scoreDivEl = document.createElement ("div")
+var score = 0;
+body.appendChild(scoreDivEl)
+scoreDivEl.appendChild(scoreEl)
+scoreEl.textContent = 'Your Score = '; 
+
+
+
 function question1 () {
-//question
+//question 1 
 var divEl = document.createElement ("div");
 var h2El = document.createElement ("h2");
 h2El.textContent = "Which of the following is NOT a wizard in the LOTR universe?";
@@ -52,23 +74,72 @@ optionsEl.appendChild(btn4);
 btn1.addEventListener('click', function() {
     body.removeChild(divEl);
     timeLeft -= 8 ;
+    question2 ();
 });
 btn2.addEventListener('click', function() {
-    window.alert("that is wrong!")
     body.removeChild(divEl);
-    timeLeft -= 8 ;});
+    timeLeft -= 8 ;
+    question2 ();
+});
 btn3.addEventListener('click', function() {
-    window.alert("that is wrong!")
     body.removeChild(divEl);
-    timeLeft -= 8 ;});
+    timeLeft -= 8 ;
+    question2 ();
+});
 btn4.addEventListener('click', function() {
-    window.alert("that is correct!");
     body.removeChild(divEl);
-//add a point to score
-  });
+    score = score + 10;
+    scoreEl.textContent = 'Your Score = ' + score ; 
+    question2 ();
+});
 }
 question1 ();
 
-// window.alert ("the previous question is over? Maybe?")
 
-}
+                    function question2 () {
+                        //question
+                        var divEl = document.createElement ("div");
+                        var h2El = document.createElement ("h2");
+                        h2El.textContent = "What was the name of Sauron's master?";
+                        divEl.appendChild(h2El);
+                        
+                        //button answers
+                        var optionsEl = document.createElement ("ol");
+                        var btn1 = document.createElement ("button");
+                        var btn2 = document.createElement ("button");
+                        var btn3 = document.createElement ("button");
+                        var btn4 = document.createElement ("button");
+                        btn1.textContent = "Melkor";
+                        btn2.textContent = "Saruman";
+                        btn3.textContent = "Illuvatar";
+                        btn4.textContent = "Manwe";
+                        //where buttun answers go
+                        body.appendChild(divEl);
+                        divEl.appendChild(optionsEl);
+                        optionsEl.appendChild(btn1);
+                        optionsEl.appendChild(btn2);
+                        optionsEl.appendChild(btn3);
+                        optionsEl.appendChild(btn4);
+                        
+                        //what button answers do 
+                        btn1.addEventListener('click', function() {
+                            body.removeChild(divEl);
+                            score = score + 10;
+                            scoreEl.textContent = 'Your Score = ' + score ; 
+                        });
+                        btn2.addEventListener('click', function() {
+                            body.removeChild(divEl);
+                            timeLeft -= 8 ;});
+                        btn3.addEventListener('click', function() {
+                            body.removeChild(divEl);
+                            timeLeft -= 8 ;});
+                        btn4.addEventListener('click', function() {
+                            body.removeChild(divEl);
+                            score = score + 10;
+                        });
+                        }
+                        
+
+                        
+                        console.log(score);
+                    }
